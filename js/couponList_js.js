9d97ffe04443a5700f8d3d395eba9bfb0d47dcb4
wendeehsu <30789244+wendeehsu@users.noreply.github.com>
2018-02-18 19:09:30 +0800

    80% finished

--- js/couponList_js.js
+++ js/couponList_js.js
@@ -1,27 +1,78 @@
-var switchBool = new Vue({
+var resource = new Vue({
 	el:'.main',
 	data:{
 		checked:false,
-		couponList:	//存用戶可用優惠券的資料
+		coupons:[
 		{
-			pizza:true,
-			nuts:false,
-			cloth:false,
-			cake:true,
-			cookie:true,
-			frozen:false,
+			num:1,
+			name:'披薩集點卡',
+			shop:'SoFree柴燒披薩',
+			detail:'可兌換免費披薩一張',
+			time:'即日起至5/8前',
+			usable:true,
+			imgUrl:'../img/sofree.jpg',
+		},{
+			num:2,
+			name:'TIME立解班',
+			shop:'OKE美語',
+			detail:'免費試聽TIME立解課程一堂',
+			time:'5/8至6月底',
+			usable:false,
+			imgUrl:'../img/okeLogo.jpg',
+		},{
+			num:3,
+			name:'單字文法書',
+			shop:'OKE美語',
+			detail:'免費單字文法書一本',
+			time:'5/8至6月底',
+			usable:false,
+			imgUrl:'../img/okeLogo.jpg',
+		},{
+			num:4,
+			name:'免費披薩一張',
+			shop:'SoFree柴燒披薩',
+			detail:'免費單字文法書一本',
+			time:'即日起至5/8前',
+			usable:true,
+			imgUrl:'../img/sofree.jpg',
+		},{
+			num:5,
+			name:'免費披薩一張',
+			shop:'SoFree柴燒披薩',
+			detail:'免費單字文法書一本',
+			time:'即日起至5/8前',
+			usable:false,
+			imgUrl:'../img/sofree.jpg',
+		},{
+			num:6,
+			name:'免費披薩一張',
+			shop:'SoFree柴燒披薩',
+			detail:'免費單字文法書一本',
+			time:'即日起至5/8前',
+			usable:true,
+			imgUrl:'../img/sofree.jpg',
 		},
-	}
-});
-
-var card = new Vue({
-	el:'#exampleModal',
-	data:{
-		title:'XX折價券',
-		imgUrl:'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/wp-cms/uploads/2017/06/i-1-sonic-burger.jpg',
-		storeName:'SoFree柴燒披薩',
-		couponInfo:'免費披薩一張',
-		deadLine:'即日起至5/8前',
-		show: true,
+		]
+	},
+	methods:{
+		check:function(k){
+			$('#back'+k).removeClass('hide');
+			$('#modal-body'+k).addClass('deleteStyle');
+			$('#row'+k).addClass('deleteStyle');
+			$('#front'+k).addClass('shrink');
+			$('#modal-content'+k).addClass('changebg');
+		},
+		dcheck:function(k){
+			$('#back'+k).addClass('hide');
+			$('#modal-body'+k).removeClass('deleteStyle');
+			$('#row'+k).removeClass('deleteStyle');
+			$('#front'+k).removeClass('shrink');
+			$('#modal-content'+k).removeClass('changebg');
+		},
+		submit:function(k){
+			this.coupons[k-1].usable = false;
+			this.dcheck(k);
+		}
 	}
 })
+
