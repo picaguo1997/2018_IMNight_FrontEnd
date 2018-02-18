648d63e86701795a2863faab0bb8c00c1616412c
He-Wei Lee <b05705006@ntu.edu.tw>
2018-02-19 02:04:37 +0800

    finish menu page (without connected to backend)

---
+++ js/menuPage.js
@@ -0,0 +1,28 @@
+let login = true;
+let cardDrawn = false;
+let discountTaken = false;
+
+let login_modal = new Vue({
+	el: '#loginModal',
+	data: {
+		message: '',
+		loggedIn: login
+	}
+})
+
+let remind_modal = new Vue({
+	el: '#remindModal',
+	data: {
+		message: '',
+		loggedIn: login,
+		drawnCard: cardDrawn,
+		takenDiscount: discountTaken
+	}
+})
+
+if (login) {
+	$('#remindModal').modal('toggle');
+}
+else {
+	$('#loginModal').modal('toggle');
+}
