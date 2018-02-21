bff72281e5cfd04855cbc862b7297d45c4d47a1d
He-Wei Lee <b05705006@ntu.edu.tw>
2018-02-21 19:54:36 +0800

    ajax get user information

--- js/menuPage.js
+++ js/menuPage.js
@@ -2,6 +2,51 @@ let login = false;
 let cardDrawn = false;
 let discountTaken = false;
 
+$(function(){
+	//post for log in
+	// $.ajax({
+	// 	type: 'POST',
+	// 	url: 'http://140.112.106.45:8000/accounts/login/',
+	// 	data: {"password":"testimnight","username":"test123"},
+	// 	success:function(data) {
+	// 		console.log(data);
+	// 	}
+	// })
+
+	// check if the user has logged in
+	$.ajax({
+		type: 'GET',
+		url: 'http://140.112.106.45:8000/accounts/check/login/',
+		xhrFields: {
+            withCredentials: true
+        },
+        success: function(data) {
+			console.log(data);
+			login = data.auth_status;
+		},
+		error: function() {
+			alert('fuck');
+		}
+	})
+
+	// if the user has logged in, check if he has drawn card or taken discount
+	if (login == true) {
+		$.ajax({
+			type: 'GET',
+			url: 'http://140.112.106.45:8000/accounts/check/daily',
+			xhrFields: {
+	            withCredentials: true
+	        },
+	        success: function(data) {
+				console.log(data);
+				cardDrawn = data.performer_drawn;
+				discountTaken = data.voucher_drawn;
+			}
+		})		
+	}
+
+})
+
 let login_modal = new Vue({
 	el: '#loginModal',
 	data: {
@@ -26,3 +71,12 @@ if (login) {
 else {
 	$('#loginModal').modal('toggle');
 }
+
+$('.enlarge').hover(
+function(){
+	$(this).css('cursor', 'pointer');
+	$(this).addClass('transition');
+}, 
+function(){
+	$(this).removeClass('transition');
+});
